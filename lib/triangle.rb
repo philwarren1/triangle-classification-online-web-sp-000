a8c5e8be4d190e403cc26dd7fb4c6c73636b2b29
Philip Warren <philwarren1@gmail.com>
2020-08-18 19:25:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,41 +1,28 @@
 class Triangle
-
- attr_accessor :side_1, :side_2, :side_3  
-  
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
-  
-  def kind 
- end
 
-    
-end
-
-class Person
-  attr_accessor :partner, :name
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def get_married(person)
-    self.partner = person
-    if person.class != Person
-      begin
-        raise PartnerError
-      rescue PartnerError => error
-          puts error.message
-      end
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
     else
-      person.partner = self
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
+
   end
- 
-  class PartnerError < StandardError
-    def message
-      "you must give the get_married method an argument of an instance of the person class!"
-    end
-  end
+
 end
 
+class TriangleError < StandardError
+end
