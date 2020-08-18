09d3977300b7730a6f2ba27e48b120a6c0a2d4c6
Philip Warren <philwarren1@gmail.com>
2020-08-18 19:16:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,3 +7,29 @@ class Triangle
     
 end
 
+class Person
+  attr_accessor :partner, :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def get_married(person)
+    self.partner = person
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+ 
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
+end
