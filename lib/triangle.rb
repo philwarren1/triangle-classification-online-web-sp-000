cc713937a48a55ad16e2944335f04cc71e7621a9
Philip Warren <philwarren1@gmail.com>
2020-08-18 19:20:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -38,3 +38,10 @@ class Person
   end
 end
 
+class User
+  attr_accessor :name, :user_name, :age, :location, :bio
+ 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+end
